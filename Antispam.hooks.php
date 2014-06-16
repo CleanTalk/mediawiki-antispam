73bc1b3723babf170c12ba1ed32a0ea4f522dbc1
Denis Shagimuratov <poluxster@gmail.com>
2014-06-16 13:48:04 +0600

    First commit.

---
+++ Antispam.hooks.php
@@ -0,0 +1,134 @@
+<?php
+
+class CTHooks {
+
+	/**
+	 * Some HTML&JS code for JavaScript test 
+	 * @param HTMLForm $form
+	 * @return bool
+	 */
+    public static function onShowEditForm( $editPage ) {
+        global $wgCTSubmitTimeLabel;
+
+        $editPage->editFormTextBottom = CTBody::AddJSCode();
+        
+        $_SESSION[$wgCTSubmitTimeLabel] = time();
+        
+        return true;
+    }
+	/**
+	 * Some HTML&JS code for JavaScript test 
+	 * @param HTMLForm $form
+	 * @return bool
+	 */
+    public static function onUserCreateForm( &$template ) {
+        global $wgCTSubmitTimeLabel;
+        
+        $template->set( 'header', CTBody::AddJSCode() );
+
+        $_SESSION[$wgCTSubmitTimeLabel] = time();
+        
+        return true;
+    }
+	
+    /**
+	 * Edit spam test 
+	 * @return bool
+	 */
+	public static function onEditFilter (  $editor, $text, $section, &$error, $summary ) {
+        global $wgCTAccessKey, $wgCTServerURL, $wgRequest, $wgCTAgent, $wgCTExtName;
+        
+        $allowEdit = true;
+
+        // Skip antispam test if editor member of special group
+        if ( $editor->getArticle()->getContext()->getUser()->isAllowed('cleantalk-bypass') ) {
+            return $allowEdit;
+        }
+
+        // The facility in which to store the query parameters
+        $ctRequest = new CleantalkRequest();
+
+        $ctRequest->auth_key = $wgCTAccessKey;
+        $ctRequest->sender_email = $editor->getArticle()->getContext()->getUser()->mEmail; 
+        $ctRequest->sender_nickname = $editor->getArticle()->getContext()->getUser()->mName;
+        $ctRequest->message = $text; 
+        $ctRequest->agent = $wgCTAgent;
+        $ctRequest->sender_ip = $wgRequest->getIP(); 
+        $ctRequest->js_on = CTBody::JSTest(); 
+        $ctRequest->submit_time = CTBody::SubmitTimeTest(); 
+
+        $ct = new Cleantalk();
+        $ct->server_url = $wgCTServerURL;
+
+        // Check
+        $ctResult = $ct->isAllowMessage($ctRequest);
+
+        // Allow edit if we have any API errors
+        if ( $ctResult->errno != 0 ) {
+            return $allowEdit;
+        }
+
+        // Disallow edit with CleanTalk comment 
+        if ($ctResult->allow == 0 && ($ctResult->spam || $ctResult->stop_queue)) {
+            $error = $ctResult->comment;
+            
+            // Converting links to wikitext format
+            $error = preg_replace("(<a\shref=\"([^\s]+)\".+>([a-f0-9]+)</a>)", "[$1 $2]", $error);
+            
+            $error = Html::openElement( 'div', array( 'class' => 'errorbox' ) ) .
+               $error . 
+               Html::closeElement( 'div' ) . "\n" .
+               Html::element( 'br', array( 'clear' => 'all' ) ) . "\n";
+        }
+       
+        if ($ctResult->inactive === 1) {
+            CTBody::SendAdminEmail( $wgCTExtName, $ctResult->comment );
+        }
+
+        return $allowEdit;
+	}
+    
+    /**
+	 * Account spam test 
+	 * @return bool
+	 */
+	public static function onAbortNewAccount ( $user, &$message ) {
+        global $wgCTAccessKey, $wgCTServerURL, $wgRequest, $wgCTAgent, $wgCTExtName;
+        
+        $allowAccount = true;
+        
+        // The facility in which to store the query parameters
+        $ctRequest = new CleantalkRequest();
+
+        $ctRequest->auth_key = $wgCTAccessKey;
+        $ctRequest->sender_email = $user->mEmail; 
+        $ctRequest->sender_nickname = $user->mName; 
+        $ctRequest->agent = $wgCTAgent;
+        $ctRequest->sender_ip = $wgRequest->getIP(); 
+        $ctRequest->js_on = CTBody::JSTest(); 
+        $ctRequest->submit_time = CTBody::SubmitTimeTest(); 
+
+        $ct = new Cleantalk();
+        $ct->server_url = $wgCTServerURL;
+
+        // Check
+        $ctResult = $ct->isAllowUser($ctRequest);
+
+        // Allow account if we have any API errors
+        if ( $ctResult->errno != 0 ) {
+            return $allowAccount;
+        }
+
+        // Disallow account with CleanTalk comment 
+        if ($ctResult->allow == 0) {
+            $allowAccount = false;
+            $message = $ctResult->comment;
+        }
+        
+        if ($ctResult->inactive === 1) {
+            CTBody::SendAdminEmail( $wgCTExtName, $ctResult->comment );
+        }
+
+        return $allowAccount;
+	}
+}
---
+++ Antispam.php
@@ -0,0 +1,87 @@
+<?php
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die;
+}
+
+require_once('cleantalk.class.php' );
+
+/**
+ * cleantalk.org is a cloud antispam service 
+ *
+ * @see http://stopforumspam.com/faq
+ * @author Denis Shagimuratov <shagimuratov@cleantalk.org>
+ * @license GPL v2 or higher
+ */
+
+/**
+ * Access key for cleantalk.org
+ * @see https://clenatalk.org/register
+ */
+$wgCTAccessKey = 'liemouse';
+
+/**
+ * Cloud URL 
+ */
+$wgCTServerURL = 'http://moderate.cleantalk.org/api2.0';
+
+/**
+ * Extension agent name and version 
+ */
+$wgCTAgent = 'mediawiki-11';
+
+/**
+ * Extension name 
+ */
+$wgCTExtName = 'Anti-spam by CleanTalk';
+
+/**
+ * Field name for JavaScript test 
+ */
+$wgCTHiddenFieldName = 'ct_checkjs';
+
+/**
+ * Field name for JavaScript test 
+ */
+$wgCTSubmitTimeLabel = 'ct_submit_time';
+
+/**
+ * Default value for hidden field 
+ */
+$wgCTHiddenFieldDefault = 0;
+
+/**
+ * Admin notificaction account ID 
+ */
+$wgCTAdminAccountId = 1;
+
+/**
+ * Admin notificactions interval in seconds
+ */
+$wgCTAdminNotificaionInteval = 10800;
+
+/**
+ * Extension settings store file 
+ */
+$wgCTDataStoreFile = __DIR__ . '/Antispam.store.dat';
+
+$wgExtensionCredits['antispam'][] = array(
+	'path' => __FILE__,
+	'name' => $wgCTExtName, 
+	'author' => 'Denis Shagimuratov',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:Antispam',
+	'descriptionmsg' => 'cleantalk-desc',
+	'version' => '1.1',
+);
+
+$wgAutoloadClasses['CTBody'] = __DIR__ . '/Antispam.body.php';
+$wgAutoloadClasses['CTHooks'] = __DIR__ . '/Antispam.hooks.php';
+
+$wgHooks['AbortNewAccount'][] = 'CTHooks::onAbortNewAccount';
+$wgHooks['UserCreateForm'][] = 'CTHooks::onUserCreateForm';
+
+$wgHooks['EditFilter'][] = 'CTHooks::onEditFilter';
+$wgHooks['EditPage::showEditForm:initial'][] = 'CTHooks::onShowEditForm';
+
+// Skip edit test for Administrators
+$wgGroupPermissions['sysop']['cleantalk-bypass'] = true;
