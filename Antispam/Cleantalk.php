054372da4783ed0273606244b6b97c0cc9de7b28
davydov <artem@ip-192-168-0-101.is74.loc>
2019-08-02 15:32:47 +0500

    ip to hostname resolve

--- Antispam/Cleantalk.php
+++ Antispam/Cleantalk.php
@@ -457,7 +457,8 @@ class Cleantalk
                         $server_host = $server['ip'];
                         $work_url = $server_host;
                     }
-                    $work_url = $url_prefix . $work_url; 
+                    $host = filter_var($work_url,FILTER_VALIDATE_IP) ? gethostbyaddr($work_url) : $work_url;
+                    $work_url = $url_prefix . $host;
                     if (isset($url_suffix)) 
                         $work_url = $work_url . $url_suffix;
                     
