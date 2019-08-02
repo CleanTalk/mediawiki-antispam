4b4f3c2c611e64f5e9bb3a705a23c90dc444e3c3
Glomberg <bazz@bk.ru>
2019-08-02 13:11:11 +0300

    Removed the key

--- Antispam/Antispam.php
+++ Antispam/Antispam.php
@@ -22,7 +22,7 @@ require_once('CleantalkSFW.php' );
  * Access key for cleantalk.org
  * @see https://clenatalk.org/register
  */
-$wgCTAccessKey = 'e6ypara8y6um';
+$wgCTAccessKey = '';
 
 /**
  * Check registrations, when there is errors with connection to service
