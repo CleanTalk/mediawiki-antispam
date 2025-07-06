6286dbe92ef6b20182d8b8a3d2090f72179a28ef
alexandergull <alex.g@cleantalk.org>
2025-07-06 22:20:16 +0500

    Code. Formatting.

--- Antispam/Antispam.body.php
+++ Antispam/Antispam.body.php
@@ -4,18 +4,18 @@ use MediaWiki\MediaWikiServices;
 
 class CTBody {
     /**
-     * Builds MD5 secret hash for JavaScript test (self::JSTest) 
-     * @return string 
+     * Builds MD5 secret hash for JavaScript test (self::JSTest)
+     * @return string
      */
     public static function getJSChallenge() {
         global $wgCTAccessKey, $wgEmergencyContact;
 
-        return md5( $wgCTAccessKey . '+' . $wgEmergencyContact ); 
-    } 
-    
+        return md5( $wgCTAccessKey . '+' . $wgEmergencyContact );
+    }
+
     /**
-     * Tests hidden field falue for secret hash 
-     * @return 0|1|null 
+     * Tests hidden field falue for secret hash
+     * @return 0|1|null
      */
     public static function JSTest() {
 
@@ -27,19 +27,19 @@ class CTBody {
          }else{
             return null;
          }
-    } 
+    }
     /**
-     * Cookie test 
-     * @return 
+     * Cookie test
+     * @return
      */
     public static function ctSetCookie() {
 
         global $wgCTAccessKey;
-        
+
         if( headers_sent() ) {
             return;
         }
-        
+
         // Cookie names to validate
         $cookie_test_value = array(
             'cookies_names' => array(),
@@ -52,7 +52,7 @@ class CTBody {
             $cookie_test_value['cookies_names'][] = 'ct_prev_referer';
             $cookie_test_value['check_value'] .= $_SERVER['HTTP_REFERER'];
         }
-        
+
         // Cookies test
         $cookie_test_value['check_value'] = md5($cookie_test_value['check_value']);
         self::apbct_cookie__set( 'ct_cookies_test', json_encode($cookie_test_value), 0, '/', $_SERVER['HTTP_HOST'], false, true, 'Lax' );
@@ -61,14 +61,14 @@ class CTBody {
     {
         global $wgCTAccessKey;
         if(isset($_COOKIE['ct_cookies_test'])){
-            
+
             $cookie_test = json_decode(stripslashes($_COOKIE['ct_cookies_test']), true);
-            
+
             $check_srting = $wgCTAccessKey;
             foreach($cookie_test['cookies_names'] as $cookie_name){
                 $check_srting .= isset($_COOKIE[$cookie_name]) ? $_COOKIE[$cookie_name] : '';
             } unset($cokie_name);
-            
+
             if($cookie_test['check_value'] == md5($check_srting)){
                 return 1;
             }else{
@@ -76,8 +76,8 @@ class CTBody {
             }
         }else{
             return null;
-        }        
-    } 
+        }
+    }
     public static function createSFWTables()
     {
         $dbr = self::getDBHandler();
@@ -128,31 +128,31 @@ class CTBody {
 
         $ct = new Cleantalk();
         $ct->server_url = $wgCTServerURL;
-        
+
         $ct_request = new CleantalkRequest;
 
         foreach ($params as $k => $v) {
             $ct_request->$k = $v;
         }
         $ct_request->auth_key = $wgCTAccessKey;
-        $ct_request->agent = $wgCTAgent; 
-        $ct_request->submit_time = isset($_COOKIE['ct_ps_timestamp']) ? time() - intval($_COOKIE['ct_ps_timestamp']) : 0; 
+        $ct_request->agent = $wgCTAgent;
+        $ct_request->submit_time = isset($_COOKIE['ct_ps_timestamp']) ? time() - intval($_COOKIE['ct_ps_timestamp']) : 0;
         $ct_request->sender_ip = CleantalkHelper::ip_get(array('real'), false);
         $ct_request->x_forwarded_for = CleantalkHelper::ip_get(array('x_forwarded_for'), false);
         $ct_request->x_real_ip       = CleantalkHelper::ip_get(array('x_real_ip'), false);
-        $ct_request->js_on = CTBody::JSTest();         
+        $ct_request->js_on = CTBody::JSTest();
         $ct_request->sender_info=json_encode(
         Array(
             'page_url' => htmlspecialchars(@$_SERVER['SERVER_NAME'].@$_SERVER['REQUEST_URI']),
             'REFFERRER' => $_SERVER['HTTP_REFERER'],
             'USER_AGENT' => $_SERVER['HTTP_USER_AGENT'],
-            'cookies_enabled' => CTBody::ctTestCookie(),            
+            'cookies_enabled' => CTBody::ctTestCookie(),
             'REFFERRER_PREVIOUS' => isset($_COOKIE['ct_prev_referer'])?$_COOKIE['ct_prev_referer']:0,
             'mouse_cursor_positions' => isset($_COOKIE['ct_pointer_data'])          ? json_decode(stripslashes($_COOKIE['ct_pointer_data']), true) : null,
             'js_timezone'            => isset($_COOKIE['ct_timezone'])              ? $_COOKIE['ct_timezone']             : null,
             'key_press_timestamp'    => isset($_COOKIE['ct_fkp_timestamp'])         ? $_COOKIE['ct_fkp_timestamp']        : null,
-            'page_set_timestamp'     => isset($_COOKIE['ct_ps_timestamp'])          ? $_COOKIE['ct_ps_timestamp']         : null,            
-        ));  
+            'page_set_timestamp'     => isset($_COOKIE['ct_ps_timestamp'])          ? $_COOKIE['ct_ps_timestamp']         : null,
+        ));
         switch ($method) {
             case 'check_message':
                 $result = $ct->isAllowMessage($ct_request);
@@ -165,16 +165,16 @@ class CTBody {
                 break;
             default:
                 return NULL;
-        } 
-        return $result;                              
-    }          
+        }
+        return $result;
+    }
     /**
-     * Adds hidden field to form for JavaScript test 
-     * @return string 
+     * Adds hidden field to form for JavaScript test
+     * @return string
      */
     public static function AddJSCode() {
         global $wgCTExtName;
-        
+
         $html = '<script>
         var ct_checkjs_val = \''.self::getJSChallenge().'\',
             d = new Date(),
@@ -256,15 +256,15 @@ class CTBody {
             window.attachEvent("mousedown", ctFunctionFirstKey);
             window.attachEvent("keydown", ctFunctionFirstKey);
         }</script>';
-        
+
         $html .= '<noscript><p><b>Please enable JavaScript to pass antispam protection!</b><br />Here are the instructions how to enable JavaScript in your web browser <a href="http://www.enable-javascript.com" rel="nofollow" target="_blank">http://www.enable-javascript.com</a>.<br />' . $wgCTExtName . '.</p></noscript>';
 
         return $html;
     }
 
     /**
-     * Sends email notificatioins to admins 
-     * @return bool 
+     * Sends email notificatioins to admins
+     * @return bool
      */
     public static function SendAdminEmail( $title, $body ) {
         global $wgCTAdminAccountId, $wgCTAdminNotificaionInteval;
